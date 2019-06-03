63e077714511076303549cd798f7a3b8d9f8297d
Lewis Nozaki <lhnozaki@gmail.com>
2019-06-02 16:50:50 -1000

    initial commit

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+// Book Constructor
+function Book(title, author, isbn) {
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+}
 
+
+// UI Constructor
+function UI() {}
+
+// Event Listeners
